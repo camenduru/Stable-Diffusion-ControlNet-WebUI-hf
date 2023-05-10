f72898362a44865e174271de1520ddf127ec60df
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-10 23:23:00 +0300

    test

--- diffusion_webui/utils/model_list.py
+++ diffusion_webui/utils/model_list.py
@@ -2,51 +2,47 @@ stable_model_list = [
     "TASUKU2023/Chilloutmix",
 ]
 
+controlnet_tile_model_list = [
+    "lllyasviel/control_v11f1e_sd15_tile",
+]
+
+controlnet_inpaint_model_list = [
+    "lllyasviel/control_v11p_sd15_inpaint",
+]
+
 stable_inpiant_model_list = [
     "TASUKU2023/Chilloutmix",
 ]
 
 controlnet_canny_model_list = [
-    "lllyasviel/sd-controlnet-canny",
     "lllyasviel/control_v11p_sd15_canny",
-    "thibaud/controlnet-sd21-canny-diffusers",
 ]
 
 controlnet_depth_model_list = [
-    "lllyasviel/sd-controlnet-depth",
     "lllyasviel/control_v11f1p_sd15_depth",
-    "thibaud/controlnet-sd21-depth-diffusers",
 ]
 
 controlnet_pose_model_list = [
-    "lllyasviel/sd-controlnet-openpose",
     "lllyasviel/control_v11p_sd15_openpose",
-    "thibaud/controlnet-sd21-openpose-diffusers",
 ]
 
 controlnet_hed_model_list = [
     "lllyasviel/sd-controlnet-hed",
-    "thibaud/controlnet-sd21-hed-diffusers",
 ]
 
 controlnet_scribble_model_list = [
-    "lllyasviel/sd-controlnet-scribble",
     "lllyasviel/control_v11p_sd15_scribble",
-    "thibaud/controlnet-sd21-scribble-diffusers",
 ]
 
 controlnet_mlsd_model_list = [
-    "lllyasviel/sd-controlnet-mlsd",
     "lllyasviel/control_v11p_sd15_mlsd",
 ]
 
 controlnet_normal_model_list = [
-    "lllyasviel/sd-controlnet-normal",
     "lllyasviel/control_v11p_sd15_normalbae",
 ]
 
 controlnet_seg_model_list = [
-    "lllyasviel/sd-controlnet-seg",
     "lllyasviel/control_v11p_sd15_seg",
 ]
 
@@ -59,7 +55,7 @@ controlnet_pix2pix_model_list = [
 ]
 
 controlnet_lineart_model_list = [
-    "ControlNet-1-1-preview/control_v11p_sd15_lineart",
+    "lllyasviel/control_v11p_sd15_lineart",
 ]
 
 controlnet_lineart_anime_model_list = [
