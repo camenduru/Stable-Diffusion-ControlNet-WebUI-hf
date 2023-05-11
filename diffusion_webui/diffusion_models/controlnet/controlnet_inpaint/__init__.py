34138fbbadc0dda7a3a44ef78d94ca3cb3c678d5
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-11 14:27:12 +0300

    spring cleaning 🧹

--- diffusion_webui/diffusion_models/controlnet/controlnet_inpaint/__init__.py
+++ diffusion_webui/diffusion_models/controlnet/controlnet_inpaint/__init__.py
@@ -1,9 +1,6 @@
 from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_canny import (
     StableDiffusionControlNetInpaintCannyGenerator,
 )
-from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_depth import (
-    StableDiffusionControlInpaintNetDepthGenerator,
-)
 from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_hed import (
     StableDiffusionControlNetInpaintHedGenerator,
 )
@@ -16,6 +13,9 @@ from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_i
 from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_scribble import (
     StableDiffusionControlNetInpaintScribbleGenerator,
 )
-from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_seg import (
-    StableDiffusionControlNetInpaintSegGenerator,
-)
+# from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_depth import (
+#     StableDiffusionControlInpaintNetDepthGenerator,
+# )
+# from diffusion_webui.diffusion_models.controlnet.controlnet_inpaint.controlnet_inpaint_seg import (
+#     StableDiffusionControlNetInpaintSegGenerator,
+# )
